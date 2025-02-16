ecb9580839202f6060a98f9708fde603d918f161
Francisco David Zarate Vasquez <francisco.zaratev@elektra.com.mx>
2025-02-15 23:47:24 -0600

    Initial Commit

---
+++ PropifyManager/PropifyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropifyManagerApp.swift
+//  PropifyManager
+//
+//  Created by Francisco David Zárate Vásquez on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PropifyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
