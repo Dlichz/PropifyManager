7dd0bf9023a2f75164d56466db38ffe78b3f1afb
Francisco David Zarate Vasquez <davidzarate33@gmail.com>
2025-04-24 00:49:59 -0600

    Se añade Login y logica de autenticación

---
+++ PropifyManager/Welcome/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  PropifyManager
+//
+//  Created by Francisco David Zárate Vásquez on 23/04/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
