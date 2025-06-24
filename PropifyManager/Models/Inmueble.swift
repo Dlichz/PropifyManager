fce109e0d6b7e58b9c9696d4f09c42f92939f878
Francisco David Zarate Vasquez <francisco.zaratev@elektra.com.mx>
2025-06-24 15:07:34 -0600

    se edita init

--- PropifyManager/Models/Inmueble.swift
+++ PropifyManager/Models/Inmueble.swift
@@ -34,6 +34,24 @@ struct Inmueble: Hashable, Identifiable, Codable {
     var aceptaMascotas: Bool
     var dimensiones: Dimensiones
     
+    init(id: UUID = UUID(), type: InmuebleType, name: String, direccion: Direccion, metrosCuadrados: Double, rentaMensual: Double, ocupado: Bool, inquilino: Inquilino?, notas: String?, images: [String], numeroRecamaras: Int, numeroBanos: Int, numeroEstacionamientos: Int, aceptaMascotas: Bool, dimensiones: Dimensiones) {
+        self.id = id
+        self.type = type
+        self.name = name
+        self.direccion = direccion
+        self.metrosCuadrados = metrosCuadrados
+        self.rentaMensual = rentaMensual
+        self.ocupado = ocupado
+        self.inquilino = inquilino
+        self.notas = notas
+        self.images = images
+        self.numeroRecamaras = numeroRecamaras
+        self.numeroBanos = numeroBanos
+        self.numeroEstacionamientos = numeroEstacionamientos
+        self.aceptaMascotas = aceptaMascotas
+        self.dimensiones = dimensiones
+    }
+    
     var defaultName: String {
         return "\(direccion.apodo ?? type.rawValue) \(direccion.calle)"
     }
@@ -53,6 +71,21 @@ struct Direccion: Hashable, Identifiable, Codable {
     let latitud: Double
     let longitud: Double
     
+    init(id: UUID = UUID(), calle: String, numeroExterior: String, numeroInterior: String?, colonia: String, municipio: String, estado: String, pais: String, codigoPostal: String, apodo: String?, latitud: Double, longitud: Double) {
+        self.id = id
+        self.calle = calle
+        self.numeroExterior = numeroExterior
+        self.numeroInterior = numeroInterior
+        self.colonia = colonia
+        self.municipio = municipio
+        self.estado = estado
+        self.pais = pais
+        self.codigoPostal = codigoPostal
+        self.apodo = apodo
+        self.latitud = latitud
+        self.longitud = longitud
+    }
+    
     var coordenadas: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitud, longitude: longitud)
     }
@@ -62,6 +95,12 @@ struct Dimensiones: Hashable, Codable {
     var largo: Double
     var ancho: Double
     var alto: Double
+    
+    init(largo: Double, ancho: Double, alto: Double) {
+        self.largo = largo
+        self.ancho = ancho
+        self.alto = alto
+    }
     
     var areaTotal: Double {
         return largo * ancho
