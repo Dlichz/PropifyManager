39c3e2661090f474c55445c2437ee17057e994f1
Francisco David Zarate Vasquez <francisco.zaratev@elektra.com.mx>
2025-06-24 19:49:44 -0600

    se añade contrato

--- PropifyManager/Models/Contrato.swift
+++ PropifyManager/Models/Contrato.swift
@@ -14,4 +14,13 @@ struct Contrato:Hashable, Identifiable, Codable {
     var fechaInicio: Date
     var fechaFin: Date
     var rentaMensual: Double
+    
+    init(id: UUID = UUID(), departamentoID: UUID, inquilinoID: UUID, fechaInicio: Date, fechaFin: Date, rentaMensual: Double) {
+        self.id = id
+        self.departamentoID = departamentoID
+        self.inquilinoID = inquilinoID
+        self.fechaInicio = fechaInicio
+        self.fechaFin = fechaFin
+        self.rentaMensual = rentaMensual
+    }
 }
